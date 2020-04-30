124e87b8ab8155798d81ea1546563cdee7cba4dd
Dave Sproson <daspr@faam.ac.uk>
2020-04-30 17:42:50 +0100

    Add a data get hook to retreive all data as json.

--- rainbot/web/urls.py
+++ rainbot/web/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('post', views.PostDataView.as_view(), name='post'),
+    path('data', views.GetDataView.as_view(), name='data')
 ]
--- rainbot/web/views.py
+++ rainbot/web/views.py
@@ -1,7 +1,7 @@
 import datetime
 
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views import View
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
@@ -12,6 +12,20 @@ from .models import RainBot, RainDrop
 class HomeView(View):
     def get(self, request):
         return render(request, 'web/index.html')
+
+
+class GetDataView(View):
+    def get(self, request):
+        rainbot = get_object_or_404(RainBot, pk=request.GET.get('rainbot'))
+        drops = rainbot.drops.all().order_by('time')
+
+        time = [d.time.timestamp() for d in drops]
+        rain = [d.data for d in drops]
+
+        return JsonResponse({
+            'time': time,
+            'rain': rain
+        })
 
 
 @method_decorator(csrf_exempt, name='dispatch')
